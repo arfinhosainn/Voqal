3d3439a38eb19cde1e264fde322f985180d59291
arfinhosainn <developerarfin@gmail.com>
2026-06-23 20:55:06 +0800

    Designed opt screen ui

--- shared/src/commonMain/kotlin/app/voqal/com/App.kt
+++ shared/src/commonMain/kotlin/app/voqal/com/App.kt
@@ -1,36 +1,32 @@
 package app.voqal.com
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.tooling.preview.Preview
 import app.voqal.com.core.designsystem.theme.VoqalTheme
-import app.voqal.com.presentation.onboarding.AddPhotoScreen
-import app.voqal.com.presentation.onboarding.AskFullNameScreen
-import org.jetbrains.compose.resources.painterResource
-
-import voqal.shared.generated.resources.Res
-import voqal.shared.generated.resources.compose_multiplatform
+import app.voqal.com.presentation.onboarding.OtpScreen
+import app.voqal.com.presentation.onboarding.components.OtpState
 
 @Composable
 @Preview
 fun App() {
     VoqalTheme {
-        var showContent by remember { mutableStateOf(false) }
-        AddPhotoScreen(
-            onEditClick = {},
-            onBack = {},
-            onContinue = { _, _ -> },
-            modifier = Modifier.fillMaxSize()
+        val focusRequesters = remember {
+            List(6) { FocusRequester() }
+        }
+
+        val fakeState = OtpState(
+            code = List(6) { null },
+            isValid = null
+        )
+
+        OtpScreen(
+            state = fakeState,
+            focusRequesters = focusRequesters,
+            onAction = {},
+            modifier = Modifier
         )
 
     }
